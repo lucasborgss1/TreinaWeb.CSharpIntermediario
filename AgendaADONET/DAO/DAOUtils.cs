15fec8ff6a4b9e0303f13d832d9ac5b49ec35c0f
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-04 10:49:13 -0300

    Add project files.

---
+++ AgendaADONET/DAO/DAOUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaADONET.DAO
+{
+    public class DAOUtils
+    {
+        public static DbConnection GetConexao()
+        {
+            DbConnection connection = new SqlConnection("Server=LUCASBORGSS;Database=TreinaWebCSharpIntermediario;Integrated Security=true;");
+            connection.Open();
+            return connection;
+        }
+
+        public static DbCommand GetComando(DbConnection conexao)
+        {
+            DbCommand comando = conexao.CreateCommand();
+            return comando;
+        }
+
+        public static DbDataReader GetDataReader(DbCommand comando)
+        {
+            return comando.ExecuteReader();
+        }
+    }
+}
